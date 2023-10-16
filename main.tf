710db8d587a7d31bca716d53c4bc0a95ec68f46b
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-10-16 02:58:42 -0400

    VPC Subnets routetables and associateion,gateways and nat gateways through modules concept and automated terraform with Docdb and load balancer+ Aurora DB

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_docdb_cluster_parameter_group" "main" {
-  family      = "docdb4.0"
+  family      = var.engine_family
   name        = "${local.name_prefix}-pg"
   description = "${local.name_prefix}-pg"
   tags = merge(local.tags, { Name = "${local.name_prefix}-pg" })
@@ -57,3 +57,12 @@ resource "aws_docdb_cluster" "main" {
   tags = merge(local.tags, {Name = "${local.name_prefix}-cluster" })
   engine_version = var.engine_version
  }
+
+resource "aws_docdb_cluster_instance" "main" {
+  count              = var.instance_count
+  identifier         = "${local.name_prefix}-cluster-instance-${count.index+1}"
+  #above index just for identification
+  cluster_identifier = aws_docdb_cluster.main.id
+  instance_class     = var.instance_class
+}
+
